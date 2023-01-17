da722907fe710340bde03ff0e8b0c893886d9345
Augusto Monteiro <augustnmonteiro@Augustos-MacBook-Pro.local>
2023-01-16 21:01:27 -0300

    Adicionando array sem indice

---
+++ Aula 3/jogo matematico/array sem indice.js
@@ -0,0 +1,30 @@
+
+let notas = [
+  10,
+  6,
+  5,
+  7,
+  9,
+  3,
+  7,
+  10,
+  10
+];
+
+
+let total = 0;
+
+// mesma coisa do for abaixo
+total = total + notas[0];
+total = total + notas[1];
+total = total + notas[2];
+total = total + notas[3];
+total = total + notas[4];
+total = total + notas[5];
+total = total + notas[6];
+total = total + notas[7];
+total = total + notas[8];
+
+for (let i=0; i<9; i++) {
+  total = total + notas[i]
+}
