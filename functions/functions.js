9c9b0268a19d30c8d7e9cf59a1fdcc3f13bdb909
Augusto Monteiro <augustnmonteiro@Augustos-MacBook-Pro.local>
2023-01-23 09:55:17 -0300

    functions

---
+++ functions/functions.js
@@ -0,0 +1,17 @@
+
+function soma(a, b) {
+  return a + b;
+}
+
+function multiplica(a, b) {
+  return a * b;
+}
+
+function subtrair(a, b) {
+  return a - b;
+}
+
+let x = soma(40, 10);
+let y = multiplica(5, 5);
+
+alert(subtrair(x, y));
